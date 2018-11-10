94933184730e858f82ac20b921ee0076ec4d5462
Andrea Maione <a.maione10@studenti.unisa.it>
2018-11-10 16:58:08 +0100

    Resizable e commenti inziali
    Aggiunta di setResizable(false) in SnakeBoard.java e rimozione dei commenti iniziali nei vari file

--- src/mathsnake/Environment.java
+++ src/mathsnake/Environment.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mathsnake;
 
-/**
- *
- * @author antonino
- */
 public interface Environment {
     public final int JP_WIDTH = 500;//dimensione del pannello 
     public final int JP_HEIGHT = 500;//dimensione del pannello 
--- src/mathsnake/SnakeBoard.java
+++ src/mathsnake/SnakeBoard.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mathsnake;
 
-/**
- *
- * @author antonino
- */
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
