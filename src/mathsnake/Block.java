751d90e73ec1fe7df8b96896b43e24ff996fed14
Antonio Coppola <antoniocoppola@Air-di-Antonio.fritz.box>
2018-11-11 12:49:01 +0100

    migliorati block e aggiunto test da modificare

--- src/mathsnake/Block.java
+++ src/mathsnake/Block.java
@@ -5,12 +5,18 @@
  */
 package mathsnake;
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import static javafx.scene.input.KeyCode.F;
 
 /**
  *
  * @author antoniocoppola
  */
 public class Block {
+    private static int WIDTH = 100;
+    private static int HEIGHT = 100;
     private int value; //indicates the value of operation
     private Operation op; //indicates the operation
     private Color color;
@@ -59,5 +65,19 @@ public class Block {
         return color;
     }
     
-    
+    public void printBlock(int x, int y,Graphics g){ //print block given x, y coordinate and graphics object, associeted to an JPanel
+       g.setColor(this.getColor());
+       g.fillRect(x, y, WIDTH, HEIGHT);
+       Font font = new Font("Arial", Font.BOLD, 17);
+       String text = this.getStrOp()+Integer.toString(this.getValue());
+       // Get the FontMetrics
+       FontMetrics metrics = g.getFontMetrics(font);
+       // Determine the X coordinate for the text
+       int textX = x + (WIDTH- metrics.stringWidth(text)) / 2;
+      // Determine the Y coordinate for the text
+       int textY = y + ((HEIGHT - metrics.getHeight()) / 2) + metrics.getAscent();
+       g.setFont(font);
+       g.setColor(Color.WHITE);
+       g.drawString(text, textX, textY);
+   }
 }
